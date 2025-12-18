1cfac45ff047d5cf1d15dab4dbcaec956dedb5eb
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2025-12-18 13:07:53 +0700

    Fix error di EntryViewModel

--- app/src/main/java/com/example/questapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/EntryViewModel.kt
@@ -8,16 +8,13 @@ import com.example.questapi.modeldata.DetailSiswa
 import com.example.questapi.modeldata.UIStateSiswa
 import com.example.questapi.modeldata.toDataSiswa
 import com.example.questapi.repositori.RepositoryDataSiswa
-import okhttp3.Response
+import retrofit2.Response
 
-class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
-        ViewModel() {
+class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
-        var uiStateSiswa by mutableStateOf(UIStateSiswa())
-            private set
-
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ):
-            Boolean {
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telepon.isNotBlank()
         }
@@ -25,19 +22,17 @@ class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
 
     fun updateUiState(detailSiswa: DetailSiswa) {
         uiStateSiswa =
-            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid =
-            validasiInput(detailSiswa))
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 
     suspend fun addSiswa() {
-        if (validasiInput()) {
-            val sip: Response<Void> = repositoryDataSiswa
-                .postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
-                if (sip.isSuccessful){
-                    printIn("Sukses Tambah Data : ${sip.message()}")
-                }else{
-                    printIn("Gagal Tambah Data : ${sip.errorBody()}")
-                }
+        if (validasiInput()){
+            val sip: Response<Void> = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if (sip.isSuccessful){
+                println("Sukses bro : ${sip.message()}")
+            }else{
+                println("Gagal bro : ${sip.errorBody()}")
+            }
         }
     }
 }
