6fd6bb37e311c51d28e78fa41ece7839a1413fe1
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2025-12-18 12:08:52 +0700

    Membuat repositori untuk data siswa

---
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,18 @@
+package com.example.questapi.repositori
+
+import com.example.questapi.apiservice.ServiceApiSiswa
+import com.example.questapi.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+):RepositoryDataSiswa{
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+    = serviceApiSiswa.postSiswa(dataSiswa)
+}
