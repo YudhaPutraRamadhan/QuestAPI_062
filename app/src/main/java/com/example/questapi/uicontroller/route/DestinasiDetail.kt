b36c91a2827461fcdb19e308d5cb4af171effd2d
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2025-12-25 13:01:21 +0700

    Menambahkan destinasi untuk detail siswa

---
+++ app/src/main/java/com/example/questapi/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,8 @@
+package com.example.questapi.uicontroller.route
+
+import com.example.questapi.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = R.string.detail_siswa
+}
